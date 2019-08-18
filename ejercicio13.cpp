0e85fde7ac5ac0365f08169174723d1393780131
mabisrror <mabisrror@utec.edu.pe>
2019-08-18 01:32:59 -0500

    agregar ejercicio 13

---
+++ ejercicio13.cpp
@@ -0,0 +1,12 @@
+
+int main() {
+
+    float base, altura;
+    cout<<"Ingrese la base del triangulo: ";
+    cin>>base;
+    cout<<"Ingrese la altura del triangulo: ";
+    cin>>altura;
+    float area = //calcular el area
+    cout<<"El area del triangulo es: "<<area<<"\n";
+
+}
