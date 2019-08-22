20638ce95cd38155cb49d240dacfede33c94c18a
Owen Atamari <32944160+theowex@users.noreply.github.com>
2019-08-22 00:39:21 -0500

    Update ejercicio9.cpp

--- ejercicio9.cpp
+++ ejercicio9.cpp
@@ -5,9 +5,10 @@ int main() {
 
     int i = 1;
     while (i <= 10) {
-        if (   ) {
+        if (i % 2 == 0) {
             //imprimir numeros pares verticalmente
+            cout<<i<<"\n";
         }
-
+        i++;
     }
 }
