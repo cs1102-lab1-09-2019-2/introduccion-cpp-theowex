5801a8dd457425ff9917fa0576644f0e68e80dff
Owen Atamari <32944160+theowex@users.noreply.github.com>
2019-08-22 00:11:39 -0500

    Update ejercicio3.cpp

--- ejercicio3.cpp
+++ ejercicio3.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+
 using std::cout;
 
 int main() {
-    for (int i = 0; i < ; i++) {
-        //imprimir los 10 numeros [0-9]
+    for (int i = 0; i < 10; i++) {
+        cout<<i<<"\n";
     }
 }
